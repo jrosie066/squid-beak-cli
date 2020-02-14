b5b5900617260812de8aa3f99429dd6e32bda628
Jamie Artin <artin@uwalumni.com>
2020-02-14 17:21:17 -0600

    added eslint; started project generation

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,18 @@
 #!/usr/bin/env node
 const program = require('commander');
-// const { initiatePage } = require('../lib/page');
+const chalk = require('chalk');
+const path = require('path');
+const pathExists = require('path-exists');
 const testQuestions = require('../lib/index');
-const print = (name) => { console.log(name, 'hi') };
+const generateProject = require('../lib/commands/generateProject');
 
+/**
+ * List of potential commands
+ * - generate page
+ * - generate component
+ * - create project
+ * 
+ */
 program
   .command('page [name]') // sub-command name
   .description('Generate React Page') // command description
@@ -16,8 +25,22 @@ program
     const useMaterial = args.material;
     const options = {
       useWrapper, useMaterial
-    }
+    };
     testQuestions();
     // initiatePage(name, options);
+  });
+
+program
+  .command('generate <projectName>')
+  .alias('g')
+  .description('Create new React Project')
+  .action((projectName) => {
+    // check if project folder already exists
+    if (pathExists.sync(path.resolve(process.cwd(), projectName))) {
+      console.log(chalk.yellow.bold(`Error! Directory ${projectName} already exist.`));
+      process.exit(1);
+    } else {
+      generateProject(projectName);
+    }
   });
 program.parse(process.argv);
